dab41f675b196ec3a920f63556cc54ce582d9e4d
gwolner <gwdm@a.recife.ifpe.edu.br>
2020-08-01 21:28:38 -0300

    Added second servlet

--- hangmanGame/src/java/servlets/PrimeiroServlet.java
+++ hangmanGame/src/java/servlets/PrimeiroServlet.java
@@ -13,17 +13,17 @@ public class PrimeiroServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            response.setContentType("text/html;charset=UTF-8");
-            try (PrintWriter out = response.getWriter()) {
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Servlet PrimeiroServlet</title>");            
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>Servlet PrimeiroServlet at " + request.getContextPath() + "</h1>");
-                out.println("</body>");
-                out.println("</html>");
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet PrimeiroServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet PrimeiroServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
     }
 
